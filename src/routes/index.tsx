ab6fb06a7f384477fae4fd683c78f8b0f1cd7db1
farooq-azam-khan <khanazamfarooq99@gmail.com>
2026-01-08 11:46:02 -0500

    init tanstack

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router";
+import {
+  Zap,
+  Server,
+  Route as RouteIcon,
+  Shield,
+  Waves,
+  Sparkles,
+} from "lucide-react";
+
+export const Route = createFileRoute("/")({ component: App });
+
+function App() {
+  return <div></div>;
+}
