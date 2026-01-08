829a8d6d78d322c2eef811eb2c4b00a2bdc5122e
farooq-azam-khan <khanazamfarooq99@gmail.com>
2026-01-08 12:06:43 -0500

    sonner added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import Header from "../components/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 import appCss from "../styles.css?url";
 
@@ -39,6 +40,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <TanStackDevtools
           config={{
             position: "bottom-right",
